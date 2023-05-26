972daf7c21d089e448660e57974c7e4176916a4e
Krivonos Sergey <SKrivonos@bellintegrator.com>
2023-05-26 22:16:32 +1000

    Добавил Docker-файлы для сборки образов

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -1,4 +1,4 @@
 docker build -t bell/gravitee-apim-management-ui:3.19.7 .
 docker build -t bell/gravitee-apim-portal-ui:3.19.7 .
-docker build -t bell/gravitee-apim-management-api:3.19.17 .
-docker build -t bell/gravitee-apim-gateway:3.19.17 .
+docker build -t bell/gravitee-apim-management-api:3.19.7 .
+docker build -t bell/gravitee-apim-gateway:3.19.7 .
